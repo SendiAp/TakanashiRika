0b9af544ea5c356c508e73979b84e3c8f0b45cff
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-02-16 18:23:02 +0700

    Update rss_sql.py

--- Takanashirika/modules/sql/rss_sql.py
+++ Takanashirika/modules/sql/rss_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Integer
 
-from Taknashirika.modules.sql import BASE, SESSION
+from Takanashirika.modules.sql import BASE, SESSION
 
 
 class RSS(BASE):
