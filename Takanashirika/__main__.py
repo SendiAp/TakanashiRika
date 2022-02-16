5d451817770f8c9727163d598ea4a25e87f27e8b
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-02-16 22:11:18 +0700

    Update __main__.py

--- Takanashirika/__main__.py
+++ Takanashirika/__main__.py
@@ -1,3 +1,4 @@
+import time
 import os
 import importlib
 import re
@@ -33,6 +34,7 @@ from Takanashirika import (
     OWNER_NAME,
     ALLOW_EXCL,
     telethn,
+    StartTime,
 )
 from Takanashirika.modules import ALL_MODULES
 from Takanashirika.modules.helper_funcs.chat_status import is_user_admin
@@ -40,6 +42,29 @@ from Takanashirika.modules.helper_funcs.misc import paginate_modules
 from Takanashirika.modules.connection import connected
 from Takanashirika.modules.connection import connect_button
 
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
 
 PM_START_TEXT = """
 Hello *{}*!
