215c31191baa7543a6ab575e8cba93fbbb08885a
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-02-16 18:23:20 +0700

    Update reporting_sql.py

--- Takanashirika/modules/sql/reporting_sql.py
+++ Takanashirika/modules/sql/reporting_sql.py
@@ -3,7 +3,7 @@ from typing import Union
 
 from sqlalchemy import Column, Integer, String, Boolean
 
-from Taknashirika.modules.sql import SESSION, BASE
+from Takanashirika.modules.sql import SESSION, BASE
 
 
 class ReportingUserSettings(BASE):
