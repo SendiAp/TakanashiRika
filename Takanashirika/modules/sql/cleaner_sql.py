f2f3a47bccf38da3c4c8ed0b86615a56f736e4ee
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-02-16 18:25:02 +0700

    Update cleaner_sql.py

--- Takanashirika/modules/sql/cleaner_sql.py
+++ Takanashirika/modules/sql/cleaner_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean
 
-from Takahashirika.modules.sql import BASE, SESSION
+from Takanashirika.modules.sql import BASE, SESSION
 
 
 class CleanerBlueTextChatSettings(BASE):
