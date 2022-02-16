fe93fb49c07dc7d2821ab007fd6c4b93bd364977
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-02-16 14:58:39 +0700

    Update log_channel.py

--- Takanashirika/modules/log_channel.py
+++ Takanashirika/modules/log_channel.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from functools import wraps
 
-from JisooX.modules.helper_funcs.misc import is_module_loaded
+from Takanashirika.modules.helper_funcs.misc import is_module_loaded
 
 FILENAME = __name__.rsplit(".", 1)[-1]
 
@@ -11,9 +11,9 @@ if is_module_loaded(FILENAME):
     from telegram.ext import CommandHandler, run_async
     from telegram.utils.helpers import escape_markdown
 
-    from JisooX import dispatcher, LOGGER, GBAN_LOGS
-    from JisooX.modules.helper_funcs.chat_status import user_admin
-    from JisooX.modules.sql import log_channel_sql as sql
+    from Takanashirika import dispatcher, LOGGER, GBAN_LOGS
+    from Takanashirika.modules.helper_funcs.chat_status import user_admin
+    from Takanashirika.modules.sql import log_channel_sql as sql
 
 
     def loggable(func):
