04f0d9b9acd9df488c085d3c720b187e44d07b16
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-02-16 18:22:11 +0700

    Update warns_sql.py

--- Takanashirika/modules/sql/warns_sql.py
+++ Takanashirika/modules/sql/warns_sql.py
@@ -3,7 +3,7 @@ import threading
 from sqlalchemy import Integer, Column, String, UnicodeText, func, distinct, Boolean
 from sqlalchemy.dialects import postgresql
 
-from Taknashirika.modules.sql import SESSION, BASE
+from Takanashirika.modules.sql import SESSION, BASE
 
 
 class Warns(BASE):
