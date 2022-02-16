fbba797ba1cfb73dfe94906d2968fadf6f840863
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-02-16 18:39:06 +0700

    Update blacklist_sql.py

--- Takanashirika/modules/sql/blacklist_sql.py
+++ Takanashirika/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText
 
-from Taknashirika.modules.sql import SESSION, BASE
+from Takanashirika.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
