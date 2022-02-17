71a62a26135d15157d65fa86032232f160492610
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-02-17 15:25:06 +0700

    Update forcesubs.py

--- Takanashirika/modules/forcesubs.py
+++ Takanashirika/modules/forcesubs.py
@@ -23,30 +23,30 @@ static_data_filter = filters.create(
 
 
 @run_async
-def _onUnMuteRequest(client, cb):
-    user_id = cb.from_user.id
-    chat_id = cb.message.chat.id
+def _onUnMuteRequest(bot: Bot, update: Update):
+    user_id = update.from_user.id
+    chat_id = update.message.chat.id
     chat_db = sql.fs_settings(chat_id)
     if chat_db:
         channel = chat_db.channel
-        chat_member = client.get_chat_member(chat_id, user_id)
+        chat_member = update.get_chat_member(chat_id, user_id)
         if chat_member.restricted_by:
-            if chat_member.restricted_by.id == (client.get_me()).id:
+            if chat_member.restricted_by.id == (update.get_me()).id:
                 try:
-                    client.get_chat_member(channel, user_id)
-                    client.unban_chat_member(chat_id, user_id)
-                    cb.message.delete()
+                    bot.get_chat_member(channel, user_id)
+                    bot.unban_chat_member(chat_id, user_id)
+                    bot.message.delete()
                     # if cb.message.reply_to_message.from_user.id == user_id:
                     # cb.message.delete()
                 except UserNotParticipant:
-                    client.answer_callback_query(
-                        cb.id,
+                    update.answer_callback_query(
+                        bot.id,
                         text=f"❗ Join our @{channel} channel and press 'Unmute Me' button.",
                         show_alert=True,
                     )
             else:
-                client.answer_callback_query(
-                    cb.id,
+                update.answer_callback_query(
+                    bot.id,
                     text="❗ You have been muted by admins due to some other reason.",
                     show_alert=True,
                 )
@@ -55,33 +55,33 @@ def _onUnMuteRequest(client, cb):
                 not client.get_chat_member(chat_id, (client.get_me()).id).status
                 == "administrator"
             ):
-                client.send_message(
+                update.send_message(
                     chat_id,
                     f"❗ **{cb.from_user.mention} is trying to UnMute himself but i can't unmute him because i am not an admin in this chat add me as admin again.**\n__#Leaving this chat...__",
                 )
 
             else:
-                client.answer_callback_query(
-                    cb.id,
+                update.answer_callback_query(
+                    bot.id,
                     text="❗ Warning! Don't press the button when you cn talk.",
                     show_alert=True,
                 )
 
 
 @run_async
-def _check_member(client, message):
-    chat_id = message.chat.id
+def _check_member(bot: Bot, update: Update):
+    chat_id = update.chat.id
     chat_db = sql.fs_settings(chat_id)
     if chat_db:
-        user_id = message.from_user.id
+        user_id = update.from_user.id
         if (
-            not client.get_chat_member(chat_id, user_id).status
+            not bot.get_chat_member(chat_id, user_id).status
             in ("administrator", "creator")
             and not user_id in SUDO_USERS
         ):
             channel = chat_db.channel
             try:
-                client.get_chat_member(channel, user_id)
+                bot.get_chat_member(channel, user_id)
             except UserNotParticipant:
                 try:
                     sent_message = message.reply_text(
@@ -105,7 +105,7 @@ def _check_member(client, message):
                             ]
                         ),
                     )
-                    client.restrict_chat_member(
+                    bot.restrict_chat_member(
                         chat_id, user_id, ChatPermissions(can_send_messages=False)
                     )
                 except ChatAdminRequired:
@@ -114,33 +114,33 @@ def _check_member(client, message):
                     )
 
             except ChatAdminRequired:
-                client.send_message(
+                bot.send_message(
                     chat_id,
                     text=f"😕 **I not an admin of @{channel} channel.**\n__Give me admin of that channel and retry.\n#Ending FSub...__",
                 )
 
 
 @run_async
-def fsub(client, message):
-    user = client.get_chat_member(message.chat.id, message.from_user.id)
+def foce_sub(bot: Bot, update: Update):
+    user = update.get_chat_member(message.chat.id, message.from_user.id)
     if user.status == "creator" or user.user.id in SUDO_USERS:
-        chat_id = message.chat.id
+        chat_id = update.chat.id
         if len(message.command) > 1:
-            input_str = message.command[1]
+            input_str = update.command[1]
             input_str = input_str.replace("@", "")
             if input_str.lower() in ("off", "no", "disable"):
                 sql.disapprove(chat_id)
-                message.reply_text("❌ **Force Subscribe is Disabled Successfully.**")
+                update.reply_text("❌ **Force Subscribe is Disabled Successfully.**")
             elif input_str.lower() in ("clear"):
                 sent_message = message.reply_text(
                     "**Unmuting all members who are muted by me...**"
                 )
                 try:
-                    for chat_member in client.get_chat_members(
+                    for chat_member in bot.get_chat_members(
                         message.chat.id, filter="restricted"
                     ):
                         if chat_member.restricted_by.id == (client.get_me()).id:
-                            client.unban_chat_member(chat_id, chat_member.user.id)
+                            bot.unban_chat_member(chat_id, chat_member.user.id)
                             time.sleep(1)
                     sent_message.edit("✅ **Unmuted all members who are muted by me.**")
                 except ChatAdminRequired:
@@ -149,35 +149,35 @@ def fsub(client, message):
                     )
             else:
                 try:
-                    client.get_chat_member(input_str, "me")
+                    bot.get_chat_member(input_str, "me")
                     sql.add_channel(chat_id, input_str)
-                    message.reply_text(
+                    bot.reply_text(
                         f"✅ **Force Subscribe is Enabled**\n__Force Subscribe is enabled, all the group members have to subscribe this [channel](https://t.me/{input_str}) in order to send messages in this group.__",
                         disable_web_page_preview=True,
                     )
                 except UserNotParticipant:
-                    message.reply_text(
+                    bot.reply_text(
                         f"😕 **Not an Admin in the Channel**\n__I am not an admin in the [channel](https://t.me/{input_str}). Add me as a admin in order to enable ForceSubscribe.__",
                         disable_web_page_preview=True,
                     )
                 except (UsernameNotOccupied, PeerIdInvalid):
-                    message.reply_text(f"❗ **Invalid Channel Username.**")
+                    bot.reply_text(f"❗ **Invalid Channel Username.**")
                 except Exception as err:
-                    message.reply_text(f"❗ **ERROR:** ```{err}```")
+                    bot.reply_text(f"❗ **ERROR:** ```{err}```")
         else:
             if sql.fs_settings(chat_id):
-                message.reply_text(
+                bot.reply_text(
                     f"✅ **Force Subscribe is enabled in this chat.**\n__For this [Channel](https://t.me/{sql.fs_settings(chat_id).channel})__",
                     disable_web_page_preview=True,
                 )
             else:
-                message.reply_text("❌ **Force Subscribe is disabled in this chat.**")
+                bot.reply_text("❌ **Force Subscribe is disabled in this chat.**")
     else:
-        message.reply_text(
+        bot.reply_text(
             "❗ **Group Creator Required**\n__You have to be the group creator to do that.__"
         )
 
 
-FORCE_HANDLER = CommandHandler("force",force)
+FORCE_HANDLER = CommandHandler("force",foce_sub)
 
 dispatcher.add_handler(FORCE_HANDLER)
