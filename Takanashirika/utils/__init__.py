c74104c1d2729dde1e4d545d8bce21c10ab98972
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-02-17 22:55:20 +0700

    Update __init__.py

--- Takanashirika/utils/__init__.py
+++ Takanashirika/utils/__init__.py
@@ -4,3 +4,4 @@ from .tools import human_to_bytes, humanbytes, md5, time_formatter
 from .thumbnails import changeImageSize, gen_thumb
 from .youtube import get_yt_info_id, get_yt_info_query, get_yt_info_query_slider
 from .url import get_url
+from .changers import time_to_seconds
