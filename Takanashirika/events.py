937acb23ce7117e83f95ba44baec6d71027e30b1
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-02-21 00:41:50 +0700

    Update events.py

--- Takanashirika/events.py
+++ Takanashirika/events.py
@@ -2,6 +2,44 @@ from telethon import events
 from Takanashirika import telethn
 
 
+def takanashirika(**args):
+    """ Register a new event. """
+    pattern = args.get('pattern', None)
+    disable_edited = args.get('disable_edited', False)
+    ignore_unsafe = args.get('ignore_unsafe', False)
+    unsafe_pattern = r'^[^/!#@\$A-Za-z]'
+    groups_only = args.get('groups_only', False)
+    trigger_on_fwd = args.get('trigger_on_fwd', False)
+    disable_errors = args.get('disable_errors', False)
+    insecure = args.get('insecure', False)
+
+    if pattern is not None and not pattern.startswith('(?i)'):
+        args['pattern'] = '(?i)' + pattern
+
+    if "disable_edited" in args:
+        del args['disable_edited']
+
+    if "ignore_unsafe" in args:
+        del args['ignore_unsafe']
+
+    if "groups_only" in args:
+        del args['groups_only']
+
+    if "disable_errors" in args:
+        del args['disable_errors']
+
+    if "trigger_on_fwd" in args:
+        del args['trigger_on_fwd']
+
+    if "insecure" in args:
+        del args['insecure']
+
+    if pattern:
+        if not ignore_unsafe:
+            args['pattern'] = pattern.replace('^.', unsafe_pattern, 1)
+
+    return decorator
+
 def register(**args):
     """ Registers a new message. """
     pattern = args.get('pattern', None)
