6a616649ef3fe71928e1b0f6edaa237c667abfc4
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2022-02-21 15:04:29 +0700

    Create started.py

---
+++ Takanashirika/modules/started.py
@@ -0,0 +1,34 @@
+import io
+import re
+import time
+from datetime import datetime
+from os import remove
+
+from Takanashirika.modules.sql.bot_starters import (
+    add_starter_to_db,
+    get_all_starters,
+    get_starter_details,
+)
+
+from Takanashirika import LOG_CHANNEL, user
+
+async def check_bot_started_users(user, event):
+    if user.id == OWNER_ID:
+        return
+    check = get_starter_details(user.id)
+    if check is None:
+        start_date = str(datetime.now().strftime("%B %d, %Y"))
+        notification = f"🔮 **#BOT_START**\n**First Name:** {_format.mentionuser(user.first_name , user.id)} \
+                \n**User ID: **`{user.id}`\
+                \n**Action: **Telah Memulai saya."
+    else:
+        start_date = check.date
+        notification = f"🔮 **#BOT_RESTART**\n**First Name:** {_format.mentionuser(user.first_name , user.id)}\
+                \n**ID: **`{user.id}`\
+                \n**Action: **Telah Me-Restart saya"
+    try:
+        add_starter_to_db(user.id, get_display_name(user), start_date, user.username)
+    except Exception as e:
+        LOGS.error(str(e))
+    if BOTLOG_CHATID:
+        await event.client.send_message(BOTLOG_CHATID, notification)
